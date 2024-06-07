cd306b58f850f7c9d3d612ed1f483b259bc3c006
RS_ict <r.stefani87.informatica@gmail.com>
2024-06-07 23:05:07 +0200

    wip

--- ml.js
+++ ml.js
@@ -1,71 +1,67 @@
-import { JSDOM } from 'jsdom';
-import jquery from 'jquery';
-import fs from 'fs';
-import axios from 'axios';
-import mime from 'mime';
+import { JSDOM } from "jsdom";
+import jquery from "jquery";
+import fs from "fs";
+import axios from "axios";
+import mime from "mime";
 
 export async function parse(htmlOrUrlOrPath) {
-    let htmlPageContent =  '';
-    if(fs.existsSync(html)){
-        return await parseFile(html);
-    }
-    else if (/^(?:[a-z]+:)?\/\//.test(html)) {
-       return await parseUrl(html);
-    }
-    else return parseCode(htmlPageContent);
-     
+  let htmlPageContent = "";
+  if (fs.existsSync(html)) {
+    return await parseFile(html);
+  } else if (/^(?:[a-z]+:)?\/\//.test(html)) {
+    return await parseUrl(html);
+  } else return parseCode(htmlPageContent);
 }
 export async function parseFile(file) {
-    let pageContent =  '';
-    let headers =  {};
-    if(fs.existsSync(html)){
-        htmlPageContent = fs.readFileSync(html, 'utf-8');
-        headers['content-type'] = mime.getType(filePath);
-    }
-    return parseCode(pageContent,headers);
+  let pageContent = "";
+  let headers = {};
+  if (fs.existsSync(html)) {
+    htmlPageContent = fs.readFileSync(html, "utf-8");
+    headers["content-type"] = mime.getType(filePath);
+  }
+  return parseCode(pageContent, headers);
 }
 export async function parseUrl(url) {
-    let pageContent =  '';
-    let headers =  {};
-    if (/^(?:[a-z]+:)?\/\//.test(html)) {
-        try {
-            response = await axios.get(url);
-            pageContent = response.data;
-            headers = response.headers;
-          } catch (error) {
-            console.error('Error fetching webpage:', error);
-            throw error; 
-          }
+  let pageContent = "";
+  let headers = {};
+  if (/^(?:[a-z]+:)?\/\//.test(html)) {
+    try {
+      response = await axios.get(url);
+      pageContent = response.data;
+      headers = response.headers;
+    } catch (error) {
+      console.error("Error fetching webpage:", error);
+      throw error;
     }
-    return parseCode(pageContent,headers);
+  }
+  return parseCode(pageContent, headers);
 }
 
 export function parseCode(code, headers = {}) {
-    const { window } = new JSDOM(code);
-  
-    if (window.document.doctype && window.document.doctype.name === 'HTML') {
-      const metaTags = window.document.querySelectorAll('head > meta');
-      for (const metaTag of metaTags) {
-        const name = metaTag.getAttribute('name');
-        const content = metaTag.getAttribute('content');
-  
-        if (name && content) {
-          headers[name] = content;
-        }
-      }
-    } else {
-      const contentType = window.document.contentType;
-      const charset = window.document.characterSet;
-      
-      if (contentType) {
-        headers['Content-Type'] = contentType;
-      }
-  
-      if (charset) {
-        headers['charset'] = charset;
+  const { window } = new JSDOM(code);
+
+  if (window.document.doctype && window.document.doctype.name === "HTML") {
+    const metaTags = window.document.querySelectorAll("head > meta");
+    for (const metaTag of metaTags) {
+      const name = metaTag.getAttribute("name");
+      const content = metaTag.getAttribute("content");
+
+      if (name && content) {
+        headers[name] = content;
       }
     }
-  
-    const $res = {window: window, $:jquery(window) , headers:headers};
+  } else {
+    const contentType = window.document.contentType;
+    const charset = window.document.characterSet;
+
+    if (contentType) {
+      headers["Content-Type"] = contentType;
+    }
+
+    if (charset) {
+      headers["charset"] = charset;
+    }
   }
-     
+
+  const $res = { window: window, $: jquery(window), headers: headers };
+}
